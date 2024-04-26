fb20510876c9475533d8e26ca71ae6ddce2d932a
Adam Bartonicek <bartonicek@gmail.com>
2024-04-26 09:02:40 +0200

    Some more minor fix to axis labels

--- packages/plotscape/lib/decorations/AxisLabels.ts
+++ packages/plotscape/lib/decorations/AxisLabels.ts
@@ -21,19 +21,20 @@ function render(this: AxisLabels, contexts: Contexts) {
   const [bottom, left, top, right] = getMargins();
   const base = scale.other!.codomain.min - context.fontsize / 2;
 
+  const { width, height } = context;
   context.clear();
 
   if (along === "x") {
-    let lastX = 0;
-    let lastW = 0;
+    let [lastX, lastW] = [0, 0];
 
     for (let i = 0; i < labels.length; i++) {
       const label = labels[i];
       const x = scale.pushforward(breaks[i]);
       const w = context.textWidth(label) + 1;
 
-      if (x < left || x > context.width - right) continue;
-      if (checkOverlap(lastX, x, lastW, w)) continue;
+      if (x < left || x > width - right || checkOverlap(lastX, x, lastW, w)) {
+        continue;
+      }
       [lastX, lastW] = [x, w];
 
       context.text(x, base, label);
@@ -41,20 +42,15 @@ function render(this: AxisLabels, contexts: Contexts) {
   } else if (along === "y") {
     let [lastY, lastH] = [0, 0];
 
-    console.log(breaks.map(scale.pushforward.bind(scale)));
-
     for (let i = 0; i < labels.length; i++) {
       const label = labels[i];
       const y = scale.pushforward(breaks[i]);
       const h = context.textHeight(label) + 5;
 
-      if (
-        y < bottom ||
-        y > context.height - top ||
-        checkOverlap(lastY, y, lastH, h)
-      )
+      if (y < bottom || y > height - top || checkOverlap(lastY, y, lastH, h)) {
         continue;
-      lastY = y;
+      }
+      [lastY, lastH] = [y, h];
 
       context.text(base, y, label);
     }
