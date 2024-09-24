659939514b9cbfd4a6010700c9c47d7ddf610bda
Adam Bartonicek <bartonicek@gmail.com>
2024-09-24 16:14:46 +0200

    Fixed initial render slowdown

--- lib/plot/axes.ts
+++ lib/plot/axes.ts
@@ -56,7 +56,7 @@ function renderSingleAxisLabels(plot: Plot, axis: `x` | `y`) {
 
       // Enforce a gap between y-axis labels and title
       while (margins[1] / 2 < w + h) {
-        marginLines[1] *= 1.01;
+        marginLines[1] *= 1.05; // Setting this lower can slow down initial render a lot
         Plot.resize(plot);
       }
 
